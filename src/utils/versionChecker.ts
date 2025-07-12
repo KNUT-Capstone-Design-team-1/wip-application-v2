9835ed6313d3200f4b3ca131f8accc910ab71a17
정규영 <jgy_98@naver.com>
2025-07-12 16:40:34 +0900

    improve : android, ios 버전 각각 불러오도록 로직 분리 및 버전 불러오는 기능 함수 따로 빼놓기

--- src/utils/versionChecker.ts
+++ src/utils/versionChecker.ts
@@ -6,6 +6,7 @@ import { getToken } from "@api/client/auth.ts";
 import axios from "axios";
 import Config from "react-native-config";
 import DeviceInfo from "react-native-device-info";
+import { isIos } from "@/utils/checker.ts";
 
 // 버전 업데이트 버튼 클릭 시 이동될 url
 const openStore = () => {
@@ -29,14 +30,9 @@ const versionToNumber = (version: string) => {
   return major * 10000 + minor * 100 + patch;
 };
 
-// 앱 버전 체크하는 함수
-export const checkAppVersion = async () => {
-  // ios 일 땐 return 시켜주기
-  if (Platform.OS === 'ios') return;
-
+const fetchLatestVersion = async () => {
   const token = getToken();
-
-  const initInfoData = await axios.get(
+  const response = await axios.get(
     Config.GOOGLE_CLOUD_INIT_INFO_URL as string,
     {
       headers: {
@@ -45,17 +41,25 @@ export const checkAppVersion = async () => {
     },
   );
 
-  const latestVersion = versionToNumber(
-    String(initInfoData.data.playStoreVersion),
-  );
+  // ios, android 각각 버전 가져오기
+  return isIos
+    ? String(response.data.appStoreVersion)
+    : String(response.data.playStoreVersion);
+};
 
+// 앱 버전 체크하는 함수
+export const checkAppVersion = async () => {
+  // ios 일 땐 return 시켜주기 ios 배포되면 주석 해제
+  if (isIos) return;
+
+  const latestVersion = versionToNumber(await fetchLatestVersion());
   const currentVersion = versionToNumber(String(DeviceInfo.getVersion()));
 
   try {
     if (latestVersion !== currentVersion) {
       Alert.alert(
         '업데이트 안내',
-        `앱이 업데이트되었습니다.\n업데이트 후 사용해주세요. 새로운 버전: ${initInfoData.data.playStoreVersion}`,
+        `앱이 업데이트되었습니다.\n업데이트 후 사용해주세요. 새로운 버전: ${latestVersion}`,
         [{ text: '업데이트 하러 가기', onPress: openStore }],
       );
     }
