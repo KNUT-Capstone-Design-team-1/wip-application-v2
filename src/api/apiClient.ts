6ece67cd00584a35d6433958bae147e834cd3e80
정규영 <jgy_98@naver.com>
2025-07-14 21:44:01 +0900

    feat : axios 인스턴스 생성

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,29 +1,19 @@
 import axiosInstance from '@api/index.ts';
-import { getToken } from '@api/client/auth.ts';
 
 export const apiClient = {
   get: async <T>(url: string, params?: object): Promise<T> => {
     const response = await axiosInstance.get<T>(url, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-        'Content-Type': 'application/json',
-      },
       params,
-      timeout: 1000 * 100,
     });
 
     return response.data;
   },
 
-  post: async <T>(url: string, data?: object): Promise<{ res: T; status: number, message: string }> => {
-    const response = await axiosInstance.post<T>(url, data, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-        'Content-Type': 'application/json',
-        apiVersion: 2,
-      },
-      timeout: 1000 * 150,
-    });
+  post: async <T>(
+    url: string,
+    data?: object,
+  ): Promise<{ res: T; status: number; message: string }> => {
+    const response = await axiosInstance.post<T>(url, data);
 
     return {
       res: response.data,
---
+++ src/api/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { getToken } from '@api/client/auth.ts';
+
+const token = getToken();
+
+const axiosInstance = axios.create({
+  baseURL: '',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+    apiVersion: 2,
+  },
+});
+
+// TODO : 7월 말 회의 후 인터셉터에 있어야 하는 부분 추가하기
+// 요청 인터셉터
+axiosInstance.interceptors.request.use();
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use();
+
+export default axiosInstance;
