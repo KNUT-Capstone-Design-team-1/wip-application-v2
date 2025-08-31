1846fe79db1af3e4f579b71da0d9739e34c54c30
정규영 <jgy_98@naver.com>
2025-08-31 21:52:59 +0900

    feat : 마크 데이터 가져오는 파일 추가

--- src/api/client/mark.ts
+++ src/api/client/mark.ts
@@ -1,14 +1,20 @@
 // @api/client/mark.ts
-import { apiClient } from '@api/apiClient.ts';
-import Config from 'react-native-config';
-import { TMarkDataResponse } from '@/types/TApiType.ts';
+import { apiClient } from '@api/apiClient';
+import { TMarkDataResponse } from '@/types/TApiType';
 
 const getMarkData = async (title: string, limit: number, page: number) => {
-  const response: TMarkDataResponse = await apiClient.get(
-    `${Config.GOOGLE_CLOUD_MARK_IMAGE_URL}?page=${page}&limit=${limit}&title=${encodeURIComponent(title)}`,
-  );
+  try {
+    const response: TMarkDataResponse = await apiClient.get(
+      `${process.env.EXPO_PUBLIC_GOOGLE_CLOUD_MARK_IMAGE_URL}?page=${page}&limit=${limit}&title=${encodeURIComponent(title)}`,
+    );
 
-  return response.data;
+    return {
+      pages: response.totalPage,
+      markData: response.data,
+    };
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export default getMarkData;
