a53f0361fcdd4294e7082e5812bf9b1fbc8a9551
정규영 <jgy_98@naver.com>
2025-08-31 21:56:36 +0900

    feat : 알약 검색하기 시 마크, 제형, 분할선, 정보 관련 데이터 적용되도록 추가

--- src/store/searchIdStore.ts
+++ src/store/searchIdStore.ts
@@ -1,18 +1,29 @@
 import { create } from 'zustand';
 import { type TPillSearchParam } from '@api/db/query';
 import { idSelectData } from '@constants/data';
+import { useMarkStore } from '@/store/markStore';
 
 interface ISearchIdStore {
   searchIdShapes: string[];
   searchIdColors: string[];
+  searchIdDosages: string[];
+  searchIdDividings: string[];
   searchIdFront: string;
   searchIdBack: string;
+  searchProductName: string;
+  searchCompanyName: string;
+  searchMark: string;
   setSearchIdShapes: (searchIdShapes: string[]) => void;
   setSearchIdColors: (searchIdColors: string[]) => void;
   setSearchIdFront: (searchIdFront: string) => void;
   setSearchIdBack: (searchIdBack: string) => void;
+  setProductName: (searchProductName: string) => void;
+  setCompanyName: (searchCompanyName: string) => void;
+  setSearchMark: (searchMarkName: string) => void;
   resetSearchId: () => void;
   getSearchIdItems: () => TPillSearchParam;
+  setDosageNames: (dosages: string[]) => void;
+  setDividingNames: (dividings: string[]) => void;
 }
 
 export const useSearchIdStore = create<ISearchIdStore>((set, get) => ({
@@ -20,30 +31,56 @@ export const useSearchIdStore = create<ISearchIdStore>((set, get) => ({
   searchIdColors: ['color0'],
   searchIdFront: '',
   searchIdBack: '',
+  searchProductName: '',
+  searchCompanyName: '',
+  searchMark: '',
+  searchIdDosages: ['dosage0'],
+  searchIdDividings: ['dividing0'],
   setSearchIdShapes: (searchIdShapes) => set({ searchIdShapes }),
   setSearchIdColors: (searchIdColors) => set({ searchIdColors }),
   setSearchIdFront: (searchIdFront) => set({ searchIdFront }),
   setSearchIdBack: (searchIdBack) => set({ searchIdBack }),
+  setProductName: (searchProductName) => set({ searchProductName }),
+  setCompanyName: (searchCompanyName) => set({ searchCompanyName }),
+  setSearchMark: (searchMark) => set({ searchMark }),
+  setDosageNames: (searchIdDosages) => set({ searchIdDosages }),
+  setDividingNames: (searchIdDividings) => set({ searchIdDividings }),
   resetSearchId: () => {
     set({
       searchIdShapes: ['shape0'],
       searchIdColors: ['color0'],
       searchIdFront: '',
       searchIdBack: '',
+      searchProductName: '',
+      searchCompanyName: '',
+      searchIdDosages: ['dosage0'],
+      searchIdDividings: ['dividing0'],
     });
+
+    useMarkStore.getState().resetSelectedMarkBase64();
   },
   getSearchIdItems: () => {
     const searchIdShapes = get().searchIdShapes;
     const searchIdColors = get().searchIdColors;
     const searchIdFront = get().searchIdFront;
     const searchIdBack = get().searchIdBack;
+    const searchProduct = get().searchProductName;
+    const searchCompany = get().searchCompanyName;
+    const searchIdDividings = get().searchIdDividings;
+    const searchMark = get().searchMark;
 
     const data: TPillSearchParam = {
       PRINT_FRONT: '',
       PRINT_BACK: '',
+      PRODUCT: '',
+      COMPANY: '',
       DRUG_SHAPE: [],
       COLOR_CLASS1: [],
       COLOR_CLASS2: [],
+      LINE_FRONT: [],
+      LINE_BACK: [],
+      CHART: [],
+      MARK: '',
     };
 
     if (searchIdFront.length > 0) {
@@ -52,6 +89,18 @@ export const useSearchIdStore = create<ISearchIdStore>((set, get) => ({
 
     if (searchIdBack.length > 0) {
       data['PRINT_BACK'] = '*' + searchIdBack.replace(/(?<=.)|(?=.)/g, '*');
+    }
+
+    if (searchProduct.length > 0) {
+      data['PRODUCT'] = '*' + searchProduct.replace(/(?<=.)|(?=.)/g, '*');
+    }
+
+    if (searchCompany.length > 0) {
+      data['COMPANY'] = '*' + searchCompany.replace(/(?<=.)|(?=.)/g, '*');
+    }
+
+    if (searchMark.length > 0) {
+      data['MARK'] = '*' + searchMark.replace(/(?<=.)|(?=.)/g, '*');
     }
 
     for (const item of idSelectData) {
@@ -74,6 +123,27 @@ export const useSearchIdStore = create<ISearchIdStore>((set, get) => ({
             if (searchIdColors.includes(val.category + val.key)) {
               data['COLOR_CLASS1']?.push(val.name);
               data['COLOR_CLASS2']?.push(val.name);
+            }
+            break;
+
+          case 'dosage':
+            if (val.category + val.key == 'dosage0') {
+              break;
+            }
+
+            if (searchIdDividings.includes(val.category + val.key)) {
+              data['CHART']?.push(val.name);
+            }
+            break;
+
+          case 'dividing':
+            if (val.category + val.key == 'dividing0') {
+              break;
+            }
+
+            if (searchIdDividings.includes(val.category + val.key)) {
+              data['LINE_FRONT']?.push(val.name);
+              data['LINE_BACK']?.push(val.name);
             }
             break;
         }
