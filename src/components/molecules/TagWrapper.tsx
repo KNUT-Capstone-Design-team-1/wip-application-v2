65184728e616cbf4b84871778eaede1a7c7eb21a
정규영 <jgy_98@naver.com>
2025-09-25 21:19:15 +0900

    feat : tag 들 감싸는 컴포넌트 추가

---
+++ src/components/molecules/TagWrapper.tsx
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Tag from '@/components/atoms/Tag';
+
+interface ITagListProps {
+  tagList: any[];
+  wrapperWidth: any;
+}
+
+const TagWrapper = ({ tagList, wrapperWidth }: ITagListProps) => {
+  return (
+    <View
+      style={[
+        styles.container,
+        { width: wrapperWidth },
+        tagList.length === 0 && { alignItems: 'center' },
+      ]}
+    >
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContent}
+      >
+        {tagList.length === 0 ? (
+          <Text style={styles.emptyText}>선택된 식별 사항이 없습니다.</Text>
+        ) : (
+          tagList.map((tag, index) => (
+            <Tag title={tag.key} value={tag.value} key={index} />
+          ))
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  emptyText: {
+    margin: 'auto',
+  },
+  scrollContent: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    display: 'flex',
+    gap: 5,
+  },
+});
+
+export default TagWrapper;
