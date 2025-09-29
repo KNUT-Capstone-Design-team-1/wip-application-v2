2d2bef43986a470e09e3dc221f6307a0a27a4da3
정규영 <jgy_98@naver.com>
2025-09-29 22:50:05 +0900

    style : 태그 끝쪽에 흐릿하게 보여지도록 수정

--- src/components/molecules/TagWrapper.tsx
+++ src/components/molecules/TagWrapper.tsx
@@ -1,5 +1,6 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Tag from '@/components/atoms/Tag';
+import { LinearGradient } from 'expo-linear-gradient';
 
 interface ITagListProps {
   tagList: any[];
@@ -15,13 +16,19 @@ const TagWrapper = ({ tagList, wrapperWidth }: ITagListProps) => {
         tagList.length === 0 && { alignItems: 'center' },
       ]}
     >
+      <LinearGradient
+        colors={['transparent', '#fff']} // 왼쪽은 진하게, 오른쪽은 희미하게
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.blurArea}
+      />
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.scrollContent}
       >
         {tagList.length === 0 ? (
-          <Text style={styles.emptyText}>선택된 식별 사항이 없습니다.</Text>
+          <Text style={styles.emptyText}>검색 조건이 선택되지 않았습니다.</Text>
         ) : (
           tagList.map((tag, index) => (
             <Tag title={tag.key} value={tag.value} key={index} />
@@ -47,6 +54,13 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     display: 'flex',
     gap: 5,
+  },
+  blurArea: {
+    position: 'absolute',
+    zIndex: 1,
+    right: 0,
+    width: 30,
+    height: '100%',
   },
 });
 
