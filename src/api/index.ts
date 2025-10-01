08077d71c887662de2fbe195ec2a37c9adf4f4b6
CaffeinCoding <jintea519@gmail.com>
2025-10-01 20:25:30 +0900

    hotfix: 요청마다 토큰을 갱신하도록 변경 - 이미지 검색 api 버전 구분용 키 이름 apiVersion > apiversion 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,40 +1,30 @@
 import axios from 'axios';
 import { getToken } from '@api/client/auth';
 
-const token = getToken();
-
 const axiosInstance = axios.create({
   baseURL: '',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
-    apiVersion: 2,
+    apiversion: 2,
   },
 });
 
 // TODO : 7월 말 회의 후 인터셉터에 있어야 하는 부분 추가하기
 // 요청 인터셉터
-axiosInstance.interceptors.request.use();
-
-// 응답 인터셉터
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      // 새로운 토큰 갱신
-      const token = getToken();
-
-      // 헤더 업데이트
-      axiosInstance.defaults.headers.Authorization = `Bearer ${token}`;
-      originalRequest.headers.Authorization = `Bearer ${token}`;
-      return axiosInstance(originalRequest);
-    }
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // 토큰 동적 갱신
+    const token = getToken();
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
     return Promise.reject(error);
   },
 );
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use();
 
 export default axiosInstance;
