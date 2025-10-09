0d0218c742dc3e935330be9cf31b6ba930527c8e
정규영 <jgy_98@naver.com>
2025-10-09 15:40:15 +0900

    feat : Home 에서 바턴시트 생성되도록 추가

--- src/components/screens/Home.tsx
+++ src/components/screens/Home.tsx
@@ -4,20 +4,36 @@ import SearchButtonList from '@/components/organisms/SearchButtonList';
 import LastSearchPill from '@/components/organisms/LastSearchPill';
 import MenuList from '@/components/organisms/MenuList';
 import TakeGuide from '@/components/organisms/TakeGuide';
-import { View, StyleSheet, BackHandler } from 'react-native';
+import BottomSheet from '@/components/molecules/BottomSheet';
+import { View, StyleSheet, BackHandler, Modal } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { gstyles } from '@/style/globalStyle';
 import Layout from '@/components/organisms/Layout';
 import { exitApp } from '@logicwind/react-native-exit-app';
 import { useScreenStore } from '@/store/screen';
+import { useNotices } from '@/hooks/useNotices';
+import { useNoticeStore } from '@store/noticeStore';
+import { useBottomSheet } from '@/hooks/useBottomSheet';
 
 const Home = (): React.JSX.Element => {
   const nav: any = useNavigation();
   const setScreen = useScreenStore((state) => state.setScreen);
+  const { getNoticeBottomSheet } = useNotices();
+  const mainBottomSheetData = useNoticeStore((state) => state.mainBottomSheetData);
+  const { isVisible, handleClose, handleNeverShowAgain, checkShouldShow } = useBottomSheet();
 
   const handleSetScreen = useCallback(() => {
     setScreen('홈');
   }, [setScreen]);
+
+  // main의 바텀시트에 필독 공지사항 데이터 세팅
+  useEffect(() => {
+    getNoticeBottomSheet();
+  }, []);
+
+  useEffect(() => {
+    checkShouldShow(mainBottomSheetData && mainBottomSheetData.length > 0);
+  }, [mainBottomSheetData]);
 
   useEffect(() => {
     nav.addListener('focus', () => handleSetScreen());
@@ -48,6 +64,14 @@ const Home = (): React.JSX.Element => {
           <MenuList />
           {/* 복용법 */}
           <TakeGuide />
+          {/* 바텀 시트 */}
+          <Modal visible={isVisible} transparent={true} animationType="fade">
+            <BottomSheet
+              data={mainBottomSheetData}
+              onClose={handleClose}
+              onNeverShowAgain={handleNeverShowAgain}
+            />
+          </Modal>
         </View>
       </View>
     </Layout.default>
