960c3978e53bdec72ec65772a8a0d9d1b858bc61
정규영 <jgy_98@naver.com>
2025-11-04 21:33:00 +0900

    chore : 하루 보지 않기 기간 정상 값으로 수정

--- src/hooks/useBottomSheet.ts
+++ src/hooks/useBottomSheet.ts
@@ -24,10 +24,10 @@ export const useBottomSheet = () => {
     ]),
   );
 
-  // 다신 보지 않기 처리
+  // 하루 보지 않기 처리
   const handleNeverShowAgain = async () => {
-    // const tomorrow = new Date().getTime() + 24 * 60 * 60 * 1000;
-    const tomorrow = new Date().getTime() + 20000;
+    const tomorrow = new Date().getTime() + 24 * 60 * 60 * 1000;
+    // const tomorrow = new Date().getTime() + 20000;
     await AsyncStorage.setItem(HIDE_NOTICE_KEY, tomorrow.toString());
     setIsVisible(false);
   };
