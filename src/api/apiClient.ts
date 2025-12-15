0b2212c83e2c7bdb2351ae3cf0c9a4997bda7055
CaffeinCoding <jintea519@gmail.com>
2025-12-16 02:45:53 +0900

    fix: Suspense와 use로 인해 재요청이 발생하던 문제 해결 - Suspense, use 제거 > useEffect로 전환 - promise 캐싱 처리 - request 취소 추가 (AbortController)

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,9 +1,15 @@
 import axiosInstance from '@api/index';
+import { AxiosRequestConfig } from 'axios';
 
 export const apiClient = {
-  get: async <T>(url: string, params?: object): Promise<T> => {
+  get: async <T>(
+    url: string,
+    params?: object,
+    config?: AxiosRequestConfig,
+  ): Promise<T> => {
     const response = await axiosInstance.get<T>(url, {
       params,
+      ...config,
     });
 
     return response.data;
@@ -12,8 +18,9 @@ export const apiClient = {
   post: async <T>(
     url: string,
     data?: object,
+    config?: AxiosRequestConfig,
   ): Promise<{ res: T; status: number; message: string }> => {
-    const response = await axiosInstance.post<T>(url, data);
+    const response = await axiosInstance.post<T>(url, data, config);
 
     return {
       res: response.data,
