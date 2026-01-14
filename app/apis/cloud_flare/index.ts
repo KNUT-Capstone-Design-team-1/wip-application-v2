61aa394abfd6d119cb744c0cbc2898916bb0e656
YongSiHun <tlgns1537@naver.com>
2026-01-14 22:57:14 +0900

    file: 실제 API 이름과 동일하게 API 호출 코드 파일명 통일

--- app/apis/cloud_flare/index.ts
+++ app/apis/cloud_flare/index.ts
@@ -1,2 +1,2 @@
-export * as NearbyPharmacyAPI from './nearby_pharmacy';
-export * as NoticeAPI from './notice';
+export * as NearbyPharmacyAPI from './wip_nearby_pharmacy';
+export * as NoticeAPI from './wip_notice';
--- app/apis/google_cloud/index.ts
+++ app/apis/google_cloud/index.ts
@@ -1,6 +1,6 @@
-export * as MarkImageAPI from './mark_image';
-export * as PillDetailAPI from './pill_search_result_detail';
-export * as PillImageFeatureExtractionAPI from './pill_image_feature_extraction';
-export * as DatabaseVersionAPI from './database_version';
-export * as PillDataTableSchemaAPI from './pill_data_table_schema';
-export * as PillDataResourceAPI from './pill_data_resource';
+export * as MarkImageAPI from './wip_mark_image';
+export * as PillDetailAPI from './wip_pill_detail';
+export * as PillImageFeatureExtractionAPI from './wip_pill_image_feature_extraction';
+export * as DatabaseVersionAPI from './wip_database_version';
+export * as PillDataTableSchemaAPI from './wip_pill_data_table_schema';
+export * as PillDataResourceAPI from './wip_pill_data_resource';
