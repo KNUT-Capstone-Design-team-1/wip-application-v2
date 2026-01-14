c2a7f2da4d792d95249b71b6d7708c5d73437540
정규영 <jgy_98@naver.com>
2026-01-14 13:12:20 +0900

    design : 이미지 및 색상코드 추가

--- app/constants/color.ts
+++ app/constants/color.ts
@@ -5,6 +5,7 @@ export const COLOR = {
   error: '#FF4960',
 };
 
+// TypeScript에서 숫자 키를 가진 객체 속성에 접근할 때는 대괄호 표기법([200])을 사용
 export const COLOR_PRIMARY = {
   100: '#32D2FF',
   200: '#006581',
@@ -16,4 +17,5 @@ export const COLOR_GRAY = {
   100: '#E1E1E1',
   200: '#B8B8B8',
   300: '#8E8E8E',
+  400: '#778385',
 };
