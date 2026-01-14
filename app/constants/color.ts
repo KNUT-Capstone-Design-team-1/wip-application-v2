3241e3d4ec88385abe60e426cb0a7055e161762e
정규영 <jgy_98@naver.com>
2026-01-14 11:32:27 +0900

    design : 색상 코드 모듈 추가

---
+++ app/constants/color.ts
@@ -0,0 +1,19 @@
+export const COLOR = {
+  white: '#FFFFFF',
+  black: '#000000',
+  normal: '#34B233',
+  error: '#FF4960',
+};
+
+export const COLOR_PRIMARY = {
+  100: '#32D2FF',
+  200: '#006581',
+  300: '#2B464A',
+  400: '#182729',
+};
+
+export const COLOR_GRAY = {
+  100: '#E1E1E1',
+  200: '#B8B8B8',
+  300: '#8E8E8E',
+};
