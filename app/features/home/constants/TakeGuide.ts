c44cf945ec72d370f35f1d4b4d39a543755c914a
정규영 <jgy_98@naver.com>
2026-01-18 22:09:22 +0900

    feat : 가이드 안내 컴포넌트 추가

---
+++ app/features/home/constants/TakeGuide.ts
@@ -0,0 +1,7 @@
+import GuideAlert from '@/assets/images/guide-alert.png';
+
+export const TAKE_GUIDE = {
+  title: '약물의 오용과 남용은 건강을 해칠 수 있습니다.',
+  guideMethod: '올바른 복용 방법 보러 가기 >',
+  icon: GuideAlert,
+};
