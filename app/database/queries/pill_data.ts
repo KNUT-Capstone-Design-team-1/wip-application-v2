27636b339cc17e6550284101381fe093fdbe4249
YongSiHun <tlgns1537@naver.com>
2026-01-19 23:56:11 +0900

    feat: 알약 목록 조회 쿼리 함수 구현
    #125

--- app/database/queries/pill_data.ts
+++ app/database/queries/pill_data.ts
@@ -1,10 +1,161 @@
 import { getDatabase } from '../sqlite';
-import { IPillData, ITableColumnSchema } from '../types';
+import {
+  IPillData,
+  IPillDataSearchParam,
+  ITableColumnSchema,
+  IWhereQueryClause,
+  TWhereQueryClauseFunc,
+} from '../types';
 import {
   ALL_PILL_DATA_COLUMNS,
+  buildWhereClause,
   getColumnPlaceholderForTableCreate,
   INSERT_BATCH_SIZE,
 } from '../util';
+
+/**
+ * pill_data 테이블 조회를 위한 WHERE param 생성
+ * @param pillData 조회할 데이터
+ * @returns
+ */
+const getPillDataWhereQuery: TWhereQueryClauseFunc = (
+  pillData: Partial<IPillDataSearchParam>,
+): Record<
+  keyof IPillDataSearchParam & 'PRINT_FRONT_EXACTLY' & 'PRINT_BACK_EXACTLY',
+  IWhereQueryClause
+> => {
+  return {
+    ITEM_SEQ: {
+      query: `ITEM_SEQ = ?`,
+      values: (itemSeq: string) => [itemSeq],
+    },
+    ENTP_SEQ: {
+      query: `ENTP_SEQ = ?`,
+      values: (entpSeq: string) => [entpSeq],
+    },
+    ITEM_NAME: {
+      query: `ITEM_NAME LIKE ?`,
+      values: (itemName: string) => [`%${itemName}%`],
+    },
+    ENTP_NAME: {
+      query: `ENTP_NAME LIKE ?`,
+      values: (entpName: string) => [`%${entpName}%`],
+    },
+    ITEM_PERMIT_DATE: {
+      query: `ITEM_PERMIT_DATE = ?`,
+      values: (itemPermitDate: string) => [itemPermitDate],
+    },
+    ETC_OTC_CODE: {
+      query: `ETC_OTC_CODE = ?`,
+      values: (etcOtcCode: string) => [etcOtcCode],
+    },
+    CHART: {
+      query: `CHART LIKE ?`,
+      values: (chart: string) => [`%${chart}%`],
+    },
+    BAR_CODE: {
+      query: `BAR_CODE = ?`,
+      values: (barCode: string) => [barCode],
+    },
+    MATERIAL_NAME: {
+      query: `MATERIAL_NAME LIKE ?`,
+      values: (materialName: string) => [`%${materialName}%`],
+    },
+    VALID_TERM: {
+      query: `VALID_TERM LIKE ?`,
+      values: (validTerm: string) => [`%${validTerm}%`],
+    },
+    STORAGE_METHOD: {
+      query: `STORAGE_METHOD LIKE ?`,
+      values: (storageMethod: string) => [`%${storageMethod}%`],
+    },
+    PACK_UNIT: {
+      query: `PACK_UNIT LIKE ?`,
+      values: (packUnit: string) => [`%${packUnit}%`],
+    },
+    MAIN_ITEM_INGR: {
+      query: `MAIN_ITEM_INGR LIKE ?`,
+      values: (mainItemIngr: string) => [`%${mainItemIngr}%`],
+    },
+    INGR_NAME: {
+      query: `INGR_NAME LIKE ?`,
+      values: (ingrName: string) => [`%${ingrName}%`],
+    },
+    ITEM_IMAGE: {
+      query: `ITEM_IMAGE = ?`,
+      values: (itemImage: string) => [itemImage],
+    },
+    // 식별문자의 경우 중간에 공백이나 특수문자가 들어갈 수 있어 %L%I%K%E% 형식으로 조회한다
+    PRINT_FRONT: {
+      query: `(PRINT_FRONT LIKE ? OR PRINT_BACK LIKE ?)`,
+      values: (printFront: string) => [
+        `%${printFront.split('').join('%')}%`,
+        `%${printFront.split('').join('%')}%`,
+      ],
+    },
+    PRINT_FRONT_EXACTLY: {
+      query: `PRINT_FRONT = ?`,
+      values: (printFront: string) => [printFront],
+    },
+    // 식별문자의 경우 중간에 공백이나 특수문자가 들어갈 수 있어 %L%I%K%E% 형식으로 조회한다
+    PRINT_BACK: {
+      query: `(PRINT_BACK LIKE ? OR PRINT_FRONT LIKE ?)`,
+      values: (printBack: string) => [
+        `%${printBack.split('').join('%')}%`,
+        `%${printBack.split('').join('%')}%`,
+      ],
+    },
+    PRINT_BACK_EXACTLY: {
+      query: `PRINT_BACK = ?`,
+      values: (printBack: string) => [printBack],
+    },
+    DRUG_SHAPE: {
+      query: `DRUG_SHAPE IN (${pillData.DRUG_SHAPE?.map(() => '?').join(', ')})`,
+      values: (drugShape: string[]) => [...drugShape],
+    },
+    COLOR_CLASS1: {
+      query: `(COLOR_CLASS1 IN (${pillData.COLOR_CLASS1?.map(() => '?').join(', ')}) 
+               OR COLOR_CLASS2 IN (${pillData.COLOR_CLASS1?.map(() => '?').join(', ')}))`,
+      values: (colorClass1: string[]) => [...colorClass1, ...colorClass1],
+    },
+    COLOR_CLASS2: {
+      query: `(COLOR_CLASS2 IN (${pillData.COLOR_CLASS2?.map(() => '?').join(', ')}) 
+               OR COLOR_CLASS1 IN (${pillData.COLOR_CLASS2?.map(() => '?').join(', ')}))`,
+      values: (colorClass2: string[]) => [...colorClass2, ...colorClass2],
+    },
+    LINE_FRONT: {
+      query: `(LINE_FRONT IN (${pillData.LINE_FRONT?.map(() => '?').join(', ')}) 
+               OR LINE_BACK IN (${pillData.LINE_FRONT?.map(() => '?').join(', ')}))`,
+      values: (lineFront: string[]) => [...lineFront, ...lineFront],
+    },
+    LINE_BACK: {
+      query: `(LINE_BACK IN (${pillData.LINE_BACK?.map(() => '?').join(', ')})
+               OR LINE_FRONT IN (${pillData.LINE_BACK?.map(() => '?').join(', ')}))`,
+      values: (lineBack: string[]) => [...lineBack, ...lineBack],
+    },
+    IMG_REGIST_TS: {
+      query: `IMG_REGIST_TS = ?`,
+      values: (imgRegistTs: string) => [imgRegistTs],
+    },
+    CLASS_NAME: {
+      query: `CLASS_NAME LIKE ?`,
+      values: (className: string) => [`%${className}%`],
+    },
+    MARK_CODE_FRONT: {
+      query: `(MARK_CODE_FRONT = ? OR MARK_CODE_BACK = ?)`,
+      values: (markCodeFront: string) => [markCodeFront, markCodeFront],
+    },
+    MARK_CODE_BACK: {
+      query: `(MARK_CODE_BACK = ? OR MARK_CODE_FRONT = ?)`,
+      values: (markCodeBack: string) => [markCodeBack, markCodeBack],
+    },
+    FORM_CODE: {
+      query: `(${pillData.FORM_CODE?.map(() => 'FORM_CODE LIKE ?').join(' AND ')})`,
+      values: (formCode: string[]) =>
+        formCode.map((formCode) => `%${formCode}%`),
+    },
+  };
+};
 
 /**
  * pill_data 테이블 유무 체크
@@ -45,7 +196,7 @@ export const createPillDataTable = async (columnData: ITableColumnSchema[]) => {
 
   const sql = `
     CREATE TABLE IF NOT EXISTS pill_data (
-      ${columnDefs.join(',\n      ')}
+      ${columnDefs.join(',\n')}
     )`;
 
   await db.execAsync(sql);
@@ -86,7 +237,27 @@ export const insertPillData = async (data: Partial<IPillData>[]) => {
 };
 
 /**
- * ID 값인 ITEM_SEQ를 기준으로 pill_data를 조회
+ * pill_data 목록 조회
+ * @param option
+ * @returns
+ */
+export const getPillDatas = async (param: Partial<IPillDataSearchParam>) => {
+  const db = await getDatabase();
+
+  const { whereClause, whereValues } = buildWhereClause(
+    getPillDataWhereQuery,
+    param,
+  );
+
+  const sql = `SELECT ${ALL_PILL_DATA_COLUMNS} FROM pill_data ${whereClause}`;
+
+  const result = await db.getAllAsync<IPillData>(sql, [...whereValues]);
+
+  return result;
+};
+
+/**
+ * ID 값인 ITEM_SEQ를 기준으로 pill_data 목록 조회
  * @param itemSeqs ITEM_SEQ 배열
  * @returns
  */
@@ -97,7 +268,8 @@ export const getPillDatasByItemSeq = async (itemSeqs: string[]) => {
 
   const db = await getDatabase();
 
-  const sql = `SELECT ${ALL_PILL_DATA_COLUMNS} FROM pill_data WHERE ITEM_SEQ IN (${itemSeqs.map(() => '?').join(', ')})`;
+  const sql = `SELECT ${ALL_PILL_DATA_COLUMNS} FROM pill_data 
+               WHERE ITEM_SEQ IN (${itemSeqs.map(() => '?').join(', ')})`;
 
   const result = await db.getAllAsync<IPillData>(sql, itemSeqs);
 
--- app/database/util.ts
+++ app/database/util.ts
@@ -1,4 +1,9 @@
-import { IPillData, ITableColumnSchema } from './types';
+import {
+  IPillData,
+  ITableColumnSchema,
+  IWhereQueryClause,
+  TWhereQueryClauseFunc,
+} from './types';
 
 export const INSERT_BATCH_SIZE = 500;
 
@@ -64,4 +69,40 @@ export const getColumnPlaceholderForTableCreate = (
   });
 
   return result;
+};
+
+/**
+ * 파라미터에 대한 WHERE 절을 생성
+ * @param whereQueryClauseFunc WHERE 절을 만들어주는 함수
+ * @param param 파라미터
+ * @returns
+ */
+export const buildWhereClause = (
+  whereQueryClauseFunc: TWhereQueryClauseFunc,
+  param: Record<string, any>,
+) => {
+  const queryClauseMap = whereQueryClauseFunc(param);
+
+  const whereParts: string[] = [];
+  const whereValues: any[] = [];
+
+  for (const key in param) {
+    const value = param[key as keyof typeof param];
+
+    if (value != null && queryClauseMap[key as keyof typeof queryClauseMap]) {
+      const queryClause = queryClauseMap[
+        key as keyof typeof queryClauseMap
+      ] as IWhereQueryClause;
+
+      whereParts.push(queryClause.query);
+
+      whereValues.push(...queryClause.values(value));
+    }
+  }
+
+  const whereClause = whereParts.length
+    ? `WHERE ${whereParts.join(' AND ')}`
+    : '';
+
+  return { whereClause, whereValues };
 };
