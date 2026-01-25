fbb2a9bac34bc2ed8e0534eee28ff7cd5931677d
정규영 <jgy_98@naver.com>
2026-01-25 17:43:56 +0900

    feat : 식별검색 각 section 의 wrapper 컴포넌트 추가

---
+++ src/features/pill_identification_search/components/molecules/identificationSection.tsx
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+import React, { ReactNode } from "react";
+import { styles } from '../../styles/molecules/identificationSection';
+
+interface IIdentificationSectionProps {
+  children: ReactNode;
+  title: string;
+  direction?: 'row' | 'column';
+  selectedIndex?: number[];
+}
+
+const IdentificationSection = ({
+  children,
+  title,
+}: IIdentificationSectionProps) => {
+  return (
+    <View style={styles.identificationSection}>
+      <Text style={styles.titleText}>{title}</Text>
+      <View style={styles.childrenContainer}>{children}</View>
+    </View>
+  );
+};
+
+export default IdentificationSection;
---
+++ src/features/pill_identification_search/styles/molecules/identificationSection.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { COLOR_GRAY } from '@/src/constants';
+
+export const styles = StyleSheet.create({
+  identificationSection: {
+    width: '100%',
+    gap: 10,
+  },
+  titleText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: COLOR_GRAY[300],
+    marginBottom: 5,
+  },
+  childrenContainer: {
+    width: '100%',
+  },
+});
