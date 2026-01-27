8a4b675052aad9fa85212faeb5cbc1d86920c42b
YongSiHun <tlgns1537@naver.com>
2026-01-27 20:56:23 +0900

    type: 통합 검색 지원을 위한 pill_data 테이블 스키마에 상세 정보 추가
    - IPillDataSearchParam 가독성 개선

--- src/services/database/types.ts
+++ src/services/database/types.ts
@@ -11,6 +11,9 @@ export interface IPillData {
   CHART: string; // 성상
   BAR_CODE: string; // 표준코드
   MATERIAL_NAME: string; // 원료성분
+  EE_DOC_DATA: string; // 효능효과
+  UD_DOC_DATA: string; // 용법용량
+  NB_DOC_DATA: string; // 주의사항
   VALID_TERM: string; // 유효기간
   STORAGE_METHOD: string; // 저장방법
   PACK_UNIT: string; // 포장단위
@@ -46,36 +49,9 @@ export interface ITableColumnSchema {
   isPK: boolean;
 }
 
-export interface IPillDataSearchParam {
-  ITEM_SEQ: string; // 품목일련번호
-  ITEM_NAME: string; // 품목명
-  ENTP_NAME: string; // 업체명
-  ENTP_SEQ: string; // 업소일련번호
-  ITEM_PERMIT_DATE: string; // 허가일자
-  ETC_OTC_CODE: string; // 전문일반
-  CHART: string; // 성상
-  BAR_CODE: string; // 표준코드
-  MATERIAL_NAME: string; // 원료성분
-  VALID_TERM: string; // 유효기간
-  STORAGE_METHOD: string; // 저장방법
-  PACK_UNIT: string; // 포장단위
-  MAIN_ITEM_INGR: string; // 주성분명
-  INGR_NAME: string; // 첨가제명
-  ITEM_IMAGE: string; // 큰제품이미지
-  PRINT_FRONT: string; // 표시앞
+export interface IPillDataSearchParam extends IPillData {
   PRINT_FRONT_EXACTLY: string; // 표시앞 일치
-  PRINT_BACK: string; // 표시뒤
   PRINT_BACK_EXACTLY: string; // 표시뒤 일치
-  DRUG_SHAPE: string[]; // 의약품제형
-  COLOR_CLASS1: string[]; // 색상앞
-  COLOR_CLASS2: string[]; // 색상뒤
-  LINE_FRONT: string[]; // 분할선앞
-  LINE_BACK: string[]; // 분할선뒤
-  IMG_REGIST_TS: string; // 이미지생성일자(약학정보원)
-  CLASS_NAME: string; // 분류명
-  MARK_CODE_FRONT: string; // 표기코드앞
-  MARK_CODE_BACK: string; // 표기코드뒤
-  FORM_CODE: string[]; // 제형코드명
 }
 
 export interface IWhereQueryClause {
