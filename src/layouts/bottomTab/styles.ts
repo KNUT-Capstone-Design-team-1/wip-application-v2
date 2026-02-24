960ab02d7b599bd966b3baf56c1a1e002152939f
정규영 <jgy_98@naver.com>
2026-02-24 17:28:28 +0900

    design : bottomTab heiht 수정

--- src/layouts/bottomTab/styles.ts
+++ src/layouts/bottomTab/styles.ts
@@ -5,7 +5,7 @@ export const styles = StyleSheet.create({
   bottomTabContainer: {
     position: 'relative',
     width: '100%',
-    height: 70,
+    height: 80,
     backgroundColor: '#E9E9E9',
   },
   bottomTabList: {
@@ -50,62 +50,5 @@ export const styles = StyleSheet.create({
   bottomTabLabelActive: {
     color: '#000000',
     fontWeight: '700',
-  },
-});
-
-export const styles2 = StyleSheet.create({
-  container: {
-    width: '90%',
-    justifyContent: 'center',
-    margin: 'auto',
-    flexDirection: 'row',
-    backgroundColor: COLOR_PRIMARY[400],
-    borderRadius: 30,
-    borderTopWidth: 1,
-    borderTopColor: '#E1E1E1',
-    paddingTop: 8,
-    paddingHorizontal: 8,
-    marginTop: -30,
-    // 그림자 효과 (iOS)
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: -4,
-    },
-    shadowOpacity: 0.15,
-    shadowRadius: 12,
-    // 그림자 효과 (Android)
-    elevation: 8,
-  },
-  tabItem: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 8,
-  },
-  centerTabItem: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 8,
-    marginTop: -30,
-  },
-  iconContainer: {
-    marginBottom: 4,
-  },
-  centerIconContainer: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 4,
-  },
-  label: {
-    fontSize: 11,
-    color: COLOR_GRAY[400],
-  },
-  activeLabel: {
-    fontWeight: '600',
   },
 });
