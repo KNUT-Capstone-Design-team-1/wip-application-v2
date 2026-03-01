8ae31a543f94f4090e33229fa6ec1c8ea2d8b291
정규영 <jgy_98@naver.com>
2026-03-01 15:06:14 +0900

    feat : setting 쪽 hook 구조 수정, 알약 개수 가져오는 util 함수 추가,

--- src/features/setting/components/SettingList.tsx
+++ src/features/setting/components/SettingList.tsx
@@ -1,14 +1,14 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import { SEARCH_LIST } from '../constants/setting_list';
-import { useRouter } from 'expo-router';
 import { ISettingListType } from '../types/setting_type';
 import { styles } from '../styles/SettingList';
 import { useEffect, useState } from 'react';
-import { loadPillStorageCount, clearPillStorage, clearRecentSearch } from '../hooks/use_setting';
+import { useSetting } from '../hooks/use_setting';
 
 const SettingList = () => {
-  const router = useRouter();
-  const [settingList, setSettingList] = useState<ISettingListType[]>(SEARCH_LIST);
+  const [settingList, setSettingList] =
+    useState<ISettingListType[]>(SEARCH_LIST);
+  const { settingListClickHandler, loadPillStorageCount } = useSetting();
 
   const loadData = async () => {
     const updatedList = await loadPillStorageCount();
@@ -19,21 +19,6 @@ const SettingList = () => {
     loadData();
   }, []);
 
-  // 각 버튼 터치 시 동작 결정해주는 함수
-  const settingListClickHandler = (list: ISettingListType) => {
-    if (list.path !== '') {
-      router.push(`/${list.path}`);
-      return;
-    }
-
-    // 기능별 처리
-    if (list.title === '보관함 초기화') {
-      clearPillStorage(setSettingList);
-    } else if (list.title === '기록 삭제') {
-      clearRecentSearch();
-    }
-  };
-
   return (
     <View style={styles.settingList}>
       {settingList.map((list: ISettingListType, index: number) => {
@@ -41,7 +26,7 @@ const SettingList = () => {
           <TouchableOpacity
             style={styles.settingItem}
             key={index}
-            onPress={() => settingListClickHandler(list)}
+            onPress={() => settingListClickHandler(list, setSettingList)}
           >
             <View style={styles.settingItemTextBox}>
               <Text style={styles.text}>{list.title}</Text>
--- src/features/setting/hooks/use_setting.ts
+++ src/features/setting/hooks/use_setting.ts
@@ -3,63 +3,93 @@ import { SEARCH_LIST } from '@/src/features/setting/constants/setting_list';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ISettingListType } from '../types/setting_type';
+import { useRouter } from 'expo-router';
+import React from 'react';
 
-export const loadPillStorageCount = async () => {
-  const count = await getPillStorage();
-  return SEARCH_LIST.map((item) => {
-    if (item.title === '보관함 초기화') {
-      return { ...item, value: `${count}개` };
+export const useSetting = () => {
+  const router = useRouter();
+
+  const settingListClickHandler = (
+    list: ISettingListType,
+    setSettingList: React.Dispatch<React.SetStateAction<ISettingListType[]>>,
+  ) => {
+    if (list.path !== '') {
+      router.push(`/${list.path}`);
+      return;
     }
-    return item;
-  });
-};
 
-// 보관함 초기화
-export const clearPillStorage = async (
-  onSuccess: (updatedList: ISettingListType[]) => void,
-) => {
-  Alert.alert('보관함 초기화', '보관함의 모든 알약을 삭제하시겠습니까?', [
-    {
-      text: '취소',
-      style: 'cancel',
-    },
-    {
-      text: '삭제',
-      style: 'destructive',
-      onPress: async () => {
-        try {
-          await AsyncStorage.removeItem('saveData');
-          const updatedList = await loadPillStorageCount(); // 개수 다시 로드
-          onSuccess(updatedList); // 화면 업데이트
-          Alert.alert('완료', '보관함이 초기화되었습니다.');
-        } catch (error) {
-          console.error('Failed to clear pill storage:', error);
-          Alert.alert('오류', '보관함 초기화에 실패했습니다.');
-        }
-      },
-    },
-  ]);
-};
+    // 기능별 처리
+    if (list.title === '보관함 초기화') {
+      clearPillStorage(setSettingList);
+    } else if (list.title === '기록 삭제') {
+      clearRecentSearch();
+    }
+  };
 
-export // 최근 검색 기록 삭제
-const clearRecentSearch = async () => {
-  Alert.alert('기록 삭제', '최근 검색한 알약 기록을 삭제하시겠습니까?', [
-    {
-      text: '취소',
-      style: 'cancel',
-    },
-    {
-      text: '삭제',
-      style: 'destructive',
-      onPress: async () => {
-        try {
-          await AsyncStorage.removeItem('recentSearch');
-          Alert.alert('완료', '검색 기록이 삭제되었습니다.');
-        } catch (error) {
-          console.error('Failed to clear recent search:', error);
-          Alert.alert('오류', '기록 삭제에 실패했습니다.');
-        }
+  const loadPillStorageCount = async () => {
+    const count = await getPillStorage();
+    return SEARCH_LIST.map((item) => {
+      if (item.title === '보관함 초기화') {
+        return { ...item, value: `${count}개` };
+      }
+      return item;
+    });
+  };
+
+  // 보관함 초기화
+  const clearPillStorage = async (
+    onSuccess: (updatedList: ISettingListType[]) => void,
+  ) => {
+    Alert.alert('보관함 초기화', '보관함의 모든 알약을 삭제하시겠습니까?', [
+      {
+        text: '취소',
+        style: 'cancel',
       },
-    },
-  ]);
+      {
+        text: '삭제',
+        style: 'destructive',
+        onPress: async () => {
+          try {
+            await AsyncStorage.removeItem('saveData');
+            const updatedList = await loadPillStorageCount(); // 개수 다시 로드
+            onSuccess(updatedList); // 화면 업데이트
+            Alert.alert('완료', '보관함이 초기화되었습니다.');
+          } catch (error) {
+            console.error('Failed to clear pill storage:', error);
+            Alert.alert('오류', '보관함 초기화에 실패했습니다.');
+          }
+        },
+      },
+    ]);
+  };
+
+  // 최근 검색 기록 삭제
+  const clearRecentSearch = async () => {
+    Alert.alert('기록 삭제', '최근 검색한 알약 기록을 삭제하시겠습니까?', [
+      {
+        text: '취소',
+        style: 'cancel',
+      },
+      {
+        text: '삭제',
+        style: 'destructive',
+        onPress: async () => {
+          try {
+            await AsyncStorage.removeItem('recentSearch');
+            Alert.alert('완료', '검색 기록이 삭제되었습니다.');
+          } catch (error) {
+            console.error('Failed to clear recent search:', error);
+            Alert.alert('오류', '기록 삭제에 실패했습니다.');
+          }
+        },
+      },
+    ]);
+  };
+
+  return {
+    settingListClickHandler,
+    loadPillStorageCount,
+    clearPillStorage,
+    clearRecentSearch,
+  };
 };
