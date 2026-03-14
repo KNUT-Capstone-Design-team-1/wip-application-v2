75e9a73a2fedda92a5f250e6c4a89f9482363b9e
정규영 <jgy_98@naver.com>
2026-03-14 15:20:44 +0900

    chore : 공지사항 관련 경로 추가

--- src/layouts/constants.ts
+++ src/layouts/constants.ts
@@ -8,4 +8,6 @@ export const PAGE_TITLES: { [key: string]: string } = {
   '/terms': '이용 약관',
   '/pill-search-result-list': '검색 결과',
   '/pill-save': '보관함',
+  '/notice': '공지사항',
+  '/notice-detail': '공지사항 상세',
 };
