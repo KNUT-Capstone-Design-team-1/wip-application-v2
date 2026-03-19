0f060d27283ba39208561cbc4a28ec37203e9cd4
정규영 <jgy_98@naver.com>
2026-03-19 23:35:44 +0900

    type : 마크 이미지 관련 타입 수정

--- src/features/pill_identification_search/types/mark_types.ts
+++ src/features/pill_identification_search/types/mark_types.ts
@@ -1,5 +1,18 @@
 export interface IMarkModalProps {
   onClose: () => void;
+  searchText: string;
+  setSearchText: (text: string) => void;
+  markDataList: MarkData[];
+  loading: boolean;
+  error: string | null;
+  handleSearch: () => void;
+  handleMarkSelect: (mark: MarkData) => void;
+  loadInitialMarks: (keyword: string) => Promise<void>;
+  currentPage: number;
+  totalPages: number;
+  currentGroup: number;
+  handlePageChange: (page: number) => void;
+  handleGroupChange: (group: number) => void;
 }
 
 export interface MarkData {
