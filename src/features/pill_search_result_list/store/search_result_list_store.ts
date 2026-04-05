b8c077f8bd59103dcb08874401614a771a63b6ea
정규영 <jgy_98@naver.com>
2026-04-05 14:35:52 +0900

    type : 알약 걸과 타입 분리

--- src/features/pill_search_result_list/store/search_result_list_store.ts
+++ src/features/pill_search_result_list/store/search_result_list_store.ts
@@ -1,20 +1,5 @@
 import { create } from 'zustand';
-import { IPillDataSearchParam } from '@/src/services/database/types';
-
-interface ISearchResultListStore {
-  searchResultData: any[];
-  isLoading: boolean;
-  searchParam: Partial<IPillDataSearchParam> | null;
-  currentPage: number;
-  hasMore: boolean;
-
-  setSearchParam: (param: any) => void;
-  setSearchResultData: (resultData: any[]) => void;
-  appendSearchResultData: (newData: any[]) => void; // 데이터 추가 (덮어쓰지 않음)
-  setIsLoading: (loading: boolean) => void;
-  getSearchResultData: () => any[];
-  getSearchParam: () => any;
-}
+import { ISearchResultListStore } from '../types/pill_search_result_list';
 
 export const useSearchResultListStore = create<ISearchResultListStore>((set, get) => ({
     searchResultData: [],
--- src/features/pill_search_result_list/types/pill_search_result_list.ts
+++ src/features/pill_search_result_list/types/pill_search_result_list.ts
@@ -1,3 +1,5 @@
+import { IPillDataSearchParam } from "@/src/services/database/types";
+
 export interface ISearchResultData {
   searchResultData: any[];
   isLoadingMore?: boolean;
@@ -6,4 +8,19 @@ export interface ISearchResultData {
 export interface IResultItemProps {
   resultItem: any;
   itemClickHandler: (seq: string, itemImage: string) => void;
+}
+
+export interface ISearchResultListStore {
+  searchResultData: any[];
+  isLoading: boolean;
+  searchParam: Partial<IPillDataSearchParam> | null;
+  currentPage: number;
+  hasMore: boolean;
+
+  setSearchParam: (param: any) => void;
+  setSearchResultData: (resultData: any[]) => void;
+  appendSearchResultData: (newData: any[]) => void; // 데이터 추가 (덮어쓰지 않음)
+  setIsLoading: (loading: boolean) => void;
+  getSearchResultData: () => any[];
+  getSearchParam: () => any;
 }
