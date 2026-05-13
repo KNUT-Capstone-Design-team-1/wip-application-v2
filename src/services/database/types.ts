30ebb7c0b39df9097ad673c8067573f0307735e7
YongSiHun <tlgns1537@naver.com>
2026-05-13 22:01:41 +0900

    schema: 알약 데이터 속성 추가

--- src/services/database/types.ts
+++ src/services/database/types.ts
@@ -52,18 +52,9 @@ export interface IConfig {
 export interface IPillData {
   ITEM_SEQ: string; // 품목일련번호
   ITEM_NAME: string; // 품목명
-  ENTP_NAME: string; // 업체명
   ENTP_SEQ: string; // 업소일련번호
-  ITEM_PERMIT_DATE: string; // 허가일자
-  ETC_OTC_CODE: string; // 전문일반
+  ENTP_NAME: string; // 업소명
   CHART: string; // 성상
-  BAR_CODE: string; // 표준코드
-  MATERIAL_NAME: string; // 원료성분
-  VALID_TERM: string; // 유효기간
-  STORAGE_METHOD: string; // 저장방법
-  PACK_UNIT: string; // 포장단위
-  MAIN_ITEM_INGR: string; // 주성분명
-  INGR_NAME: string; // 첨가제명
   ITEM_IMAGE: string; // 큰제품이미지
   PRINT_FRONT: string; // 표시앞
   PRINT_BACK: string; // 표시뒤
@@ -72,11 +63,55 @@ export interface IPillData {
   COLOR_CLASS2: string; // 색상뒤
   LINE_FRONT: string; // 분할선앞
   LINE_BACK: string; // 분할선뒤
+  LENGTH_LONG: string; // 크기장축
+  LENGTH_SHORT: string; // 크기단축
+  LENGTH_THICK: string; // 크기두께
   IMG_REGIST_TS: string; // 이미지생성일자(약학정보원)
+  CLASS_NO: string; // 분류번호
   CLASS_NAME: string; // 분류명
+  ETC_OTC_CODE: string; // 전문일반구분
+  ITEM_PERMIT_DATE: string; // 품목허가일자
+  FORM_CODE: string; // 제형코드명
+  DRUG_SHAPE_FRONT: string; // 표기내용앞
+  DRUG_SHAPE_BACK: string; // 표기내용뒤
+  MARK_IMAGE_FRONT: string; // 표기이미지앞
+  MARK_IMAGE_BACK: string; // 표기이미지뒤
   MARK_CODE_FRONT: string; // 표기코드앞
   MARK_CODE_BACK: string; // 표기코드뒤
-  FORM_CODE: string; // 제형코드명
+  CHANGE_DATE: string; // 변경일자
+  BUSINESS_LICENCE_NUMBER: string; // 사업자번호
+  ITEM_ENG_NAME: string; // 품목영문명
+  COVERAGE_ENG_NAME: string; // 보험코드
+  BAR_CODE: string; // 표준코드
+  APPROVAL_TYPE: string; // 허가/신고구분
+  CANCEL_STATUS: string; // 취소상태
+  CANCEL_DATE: string; // 취소일자
+  ENTP_ENG_NAME: string; // 업체 영문명
+  ENTP_PERMIT_NO: string; // 업체허가번호
+  MATERIAL_NAME: string; // 원료성분
+  MATERIAL_ENG_NAME: string; // 영문성분명
+  EE_DOC_DATA: string; // 효능효과
+  UD_DOC_DATA: string; // 용법용량
+  NB_DOC_DATA: string; // 주의사항
+  ATT_DOC_DATA: string; // 첨부문서
+  STORAGE_METHOD: string; // 저장방법
+  REEXAM_TARGET_YN: string; // 재심사대상
+  REEXAM_CONT: string; // 재심사기간
+  VALID_TERM: string; // 유효기간
+  PACK_UNIT: string; // 포장단위
+  INSURANCE_CODE: string; // 보험코드
+  DRUG_CLASS: string; // 마약류분류
+  FINISH_MATERIAL_YN: string; // 완제원료구분
+  NEW_DRUG_YN: string; // 신약여부
+  INDUTY_CODE: string; // 업종구분
+  CHANGE_CONTENT: string; // 변경내용
+  TOTAL_CONTENT: string; // 총량
+  MAIN_ITEM_INGR: string; // 주성분명
+  INGR_NAME: string; // 첨가제명
+  ATC_CODE: string; // ATC코드
+  ENTP_BIZ_NO: string; // 사업자번호
+  RARE_DRUG_YN: string; // 희귀의약품여부
+  OEM_ENTP_NAME: string; // 위탁제조업체
 }
 
 /**
