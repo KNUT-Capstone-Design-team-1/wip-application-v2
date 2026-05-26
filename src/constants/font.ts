967bf7816900951834208302627fccbed8d0b21d
Jtea <jintea519@gmail.com>
2026-05-26 22:32:30 +0900

    feat: font 추가 - 여기어때 잘난체 - PaperLogy

---
+++ src/constants/font.ts
@@ -0,0 +1,17 @@
+import { TextStyle } from 'react-native';
+
+export const FONT_PAPERLOGY_WEIGHT = {
+  thin: { fontFamily: 'Paperlogy', fontWeight: 100 } as TextStyle,
+  extraLight: { fontFamily: 'Paperlogy', fontWeight: 200 } as TextStyle,
+  light: { fontFamily: 'Paperlogy', fontWeight: 300 } as TextStyle,
+  regular: { fontFamily: 'Paperlogy', fontWeight: 400 } as TextStyle,
+  medium: { fontFamily: 'Paperlogy', fontWeight: 500 } as TextStyle,
+  semiBold: { fontFamily: 'Paperlogy', fontWeight: 600 } as TextStyle,
+  bold: { fontFamily: 'Paperlogy', fontWeight: 700 } as TextStyle,
+  extraBold: { fontFamily: 'Paperlogy', fontWeight: 800 } as TextStyle,
+  black: { fontFamily: 'Paperlogy', fontWeight: 900 } as TextStyle,
+};
+
+export const FONT_JALNAN2_WEIGHT = {
+  regular: { fontFamily: 'Jalnan2', fontWeight: 400 } as TextStyle,
+};
