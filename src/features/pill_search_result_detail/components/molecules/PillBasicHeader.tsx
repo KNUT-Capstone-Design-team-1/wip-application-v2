d42829e0d545608ef30b36395079c38f0c6b1351
CaffeinCoding <jintea519@gmail.com>
2026-06-21 20:45:02 +0900

    fix & feat: 상세화면에서 저장 버튼이 밀리는 문제 수정 - 제품명 영역에 ScrollView 와 그라데이션을 적용

--- src/features/pill_search_result_detail/components/molecules/PillBasicHeader.tsx
+++ src/features/pill_search_result_detail/components/molecules/PillBasicHeader.tsx
@@ -1,8 +1,9 @@
 import { memo } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { styles } from '../../styles/molecules/PillBasicHeader';
 import { Bookmark } from 'lucide-react-native';
 import { fontPx } from '@utils/responsive';
+import { LinearGradient } from 'expo-linear-gradient';
 
 interface IPillBasicHeaderProps {
   itemName: string;
@@ -18,20 +19,28 @@ const PillBasicHeader = ({
   const itemNames = itemName.split(/(?=\()/, 2);
 
   return (
-    <View style={styles.nameWrapper}>
-      <View>
-        <Text style={styles.name} numberOfLines={1}>
-          {itemNames[0]}
-        </Text>
-        {itemNames[1] && (
-          <Text
-            style={[styles.name, { fontSize: fontPx(18) }]}
-            numberOfLines={1}
-          >
-            {itemNames[1]}
-          </Text>
-        )}
-      </View>
+    <View style={styles.headerWrapper}>
+      <ScrollView
+        style={styles.nameScrollWrapper}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.nameViewWrapper}>
+          <Text style={styles.name}>{itemNames[0].trim()}</Text>
+          {itemNames[1] && (
+            <Text style={[styles.name, { fontSize: fontPx(18) }]}>
+              {itemNames[1]}
+            </Text>
+          )}
+        </View>
+      </ScrollView>
+      <LinearGradient
+        colors={['transparent', 'white']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        style={styles.gradiant}
+        pointerEvents="none"
+      />
       <TouchableOpacity style={styles.saveButton} onPress={onSaveToggle}>
         <Bookmark
           size={fontPx(24)}
--- src/features/pill_search_result_detail/styles/molecules/PillBasicHeader.ts
+++ src/features/pill_search_result_detail/styles/molecules/PillBasicHeader.ts
@@ -1,19 +1,27 @@
 import { StyleSheet } from 'react-native';
 import { px, fontPx } from '@utils/responsive';
+import { COLOR } from '@constants/color';
 
 export const styles = StyleSheet.create({
-  nameWrapper: {
+  headerWrapper: {
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: px(16),
   },
+  nameScrollWrapper: { marginRight: px(8) },
+  nameViewWrapper: { paddingRight: px(8) },
   name: {
-    color: '#000',
+    color: COLOR['black'],
     flex: 1,
     fontSize: fontPx(22),
     fontWeight: '700',
-    paddingRight: px(20),
+  },
+  gradiant: {
+    position: 'absolute',
+    right: 0,
+    width: '30%',
+    height: '100%',
   },
   saveButton: {},
 });
