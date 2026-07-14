d0b5dbb3fafdab5790a26240e33d63bced68629f
YongSiHun <tlgns1537@naver.com>
2026-07-14 20:40:38 +0900

    build: ipa 파일 앱스토어에 업로드 할 수 있도록 스크립트 추가

---
+++ scripts/submit_ios.js
@@ -0,0 +1,12 @@
+const { execSync } = require('node:child_process');
+
+const ipa = process.argv[2];
+
+if (!ipa) {
+  console.error('Usage: yarn submit <ipa-file>');
+  process.exit(1);
+}
+
+execSync(`eas submit --platform ios --path ${ipa}`, {
+  stdio: 'inherit',
+});
