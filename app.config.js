64b37ab62a028457feb6860686e1731c8eefc8ff
YongSiHun <tlgns1537@naver.com>
2026-08-06 19:48:23 +0900

    build: 안드로이드 배포 시 광고 미표시 가능성 문제 수정

--- app.config.js
+++ app.config.js
@@ -88,6 +88,7 @@ export default {
         'android.permission.CAMERA',
         'android.permission.ACCESS_COARSE_LOCATION',
         'android.permission.ACCESS_FINE_LOCATION',
+        'com.google.android.gms.permission.AD_ID',
       ],
 
       blockedPermissions: [
