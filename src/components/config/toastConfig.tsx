6acb19233256ce65b0fbb3093dbc663ea401d0b6
YongSiHun <tlgns1537@naver.com>
2026-09-03 01:17:31 +0900

    improve: 토스트 색상 통일

--- src/components/config/toastConfig.tsx
+++ src/components/config/toastConfig.tsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import Toast from '@components/common/Toast';
-import { COLOR } from '@constants/color';
 import { ToastConfigParams } from 'react-native-toast-message';
 
+// 앱 전역 토스트 메시지 스타일 설정 (전체 회색/다크 톤으로 통일)
 const toastConfig = {
   success: (props: ToastConfigParams<any>) => {
-    return <Toast {...props} backgroundColor={COLOR['normal']} />;
+    return <Toast {...props} />;
   },
   error: (props: ToastConfigParams<any>) => {
-    return <Toast {...props} backgroundColor={COLOR['error']} />;
+    return <Toast {...props} />;
+  },
+  info: (props: ToastConfigParams<any>) => {
+    return <Toast {...props} />;
   },
   default: (props: ToastConfigParams<any>) => {
     return <Toast {...props} />;
