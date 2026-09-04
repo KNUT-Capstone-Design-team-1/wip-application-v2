14f3268d2636662f2c95c5313f163aca664a7519
2mukee <33280934+YongSiHun@users.noreply.github.com>
2026-09-04 04:02:31 +0000

    improve: 데이터베이스 업데이트 기능 3티어 아키텍처 적용

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,2 @@
-export * as DatabaseUpdateService from './database_update';
+export { databaseUpdateService as DatabaseUpdateService } from '@features/database_update/services/database_update_service';
 export * as AppConfigService from './app_config';
