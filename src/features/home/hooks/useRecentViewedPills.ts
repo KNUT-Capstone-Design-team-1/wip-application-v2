bdb77b1bd2718f9dc2da93b23d000b141df91bbd
2mukee <33280934+YongSiHun@users.noreply.github.com>
2026-09-04 05:57:32 +0000

    fix: 홈 화면 진입 시 앱이 팅기는 현상 수정

--- src/features/home/hooks/useRecentViewedPills.ts
+++ src/features/home/hooks/useRecentViewedPills.ts
@@ -1,8 +1,11 @@
 import { useRecentViewedPillStore } from '@store/recent_viewed_pill_store';
+import { useShallow } from 'zustand/react/shallow';
 
 export const useRecentViewedPills = () =>
-  useRecentViewedPillStore((state) => ({
-    recentViewedPills: state.recentViewedPills,
-    loadRecentViewedPills: state.getRecentViewedPills,
-    deleteRecentViewedPill: state.deleteRecentViewed,
-  }));
+  useRecentViewedPillStore(
+    useShallow((state) => ({
+      recentViewedPills: state.recentViewedPills,
+      loadRecentViewedPills: state.getRecentViewedPills,
+      deleteRecentViewedPill: state.deleteRecentViewed,
+    })),
+  );
