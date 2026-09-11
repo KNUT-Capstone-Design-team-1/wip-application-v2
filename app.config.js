8be3155dd1b77c4e67e6e5f9c807ed8b18385e88
YongSiHun <tlgns1537@naver.com>
2026-09-12 00:28:04 +0900

    build: version update

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ export default {
     slug: 'whatispill',
     scheme: 'whatispill',
     owner: 'mustbemadness',
-    version: '3.3.1',
+    version: '3.3.2',
     orientation: 'portrait',
     icon: './assets/icons/android-adaptive-icon.png',
 
